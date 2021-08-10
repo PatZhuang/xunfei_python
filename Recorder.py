a0bd114f74efb8ed93005dc16727b466776a0b33
pat_zhuang <pat_zhuang@outlook.com>
2021-08-11 00:27:34 +0800

    add vad to recorder

--- Recorder.py
+++ Recorder.py
@@ -2,6 +2,8 @@ import sounddevice as sd
 import soundfile as sf
 
 import threading
+import webrtcvad
+import time
 
 class Recorder(object):
     
@@ -20,27 +22,56 @@ class Recorder(object):
                                          dtype=self.dtype,
                                          channels=self.channels,
                                          latency=0.1)
-        # self.start()
         print("Recorder initialized")
+        self.start()
     
     def start(self):
-        self.istream.start()
+        while self.istream.stopped:
+            self.istream.start()
+            time.sleep(0.1)
         print("* start recording")
         
     def stop(self):
         self.istream.stop()
         print("* stop recording")
         
-    def get_record_audio(self, duration=1):
-        if self.istream.stopped:
-            self.istream.start()
-        length = duration * self.samplerate
+    def get_record_audio(self, duration=1000):
+        while self.istream.stopped:
+            self.start()
+            
+        length = duration // 1000 * self.samplerate
         frames = []
-        for _ in range(0, int(length / self.chunk)):
+        for _ in range(0, max(int(length / self.chunk), 1)):
             data, overflowed = self.istream.read(self.chunk)
             frames.append(data)
         
         return b''.join(frames)
+    
+    def get_record_audio_with_len(self, frame_len):
+        # 因为采样率是 16bit，所以返回的长度其实是 frame_len * 2
+        return self.istream.read(frame_len)[0]
+    
+    def get_record_audio_with_vad(self, duration=10000, vad_bos=5000, vad_eos=2000):
+        vad = webrtcvad.Vad(3)
+        frame_duration = 20
+        frames = b''
+        has_spoken = False
+        
+        for i in range(duration // frame_duration):
+            time.sleep(0.02)
+            frame = self.get_record_audio_with_len(frame_len=self.samplerate // 1000 * frame_duration)
+            
+            frames += frame
+            if vad.is_speech(frame, self.samplerate):
+                has_spoken = True
+            else:
+                if not has_spoken:
+                    vad_bos -= frame_duration
+                else:
+                    vad_eos -= frame_duration
+            if not vad_bos or not vad_eos:
+                break
+        return frames, has_spoken
     
     def play_file(self, filename):
         event = threading.Event()
@@ -92,3 +123,7 @@ class Recorder(object):
         
         print("Recorder deleted")
         return
+
+if __name__ == '__main__':
+    r = Recorder()
+    r.get_record_audio_with_vad(duration=5000)
