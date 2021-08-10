11575aea5318b4caa948f043b97cd49224b01f50
pat_zhuang <pat_zhuang@outlook.com>
2021-08-11 00:28:24 +0800

    add AIUI_webapi

--- AIUI_webapi.py
+++ AIUI_webapi.py
@@ -1,17 +1,21 @@
 #-*- coding: utf-8 -*-
+
 import requests
 import time
 import hashlib
 import base64
 from rich import print
 import json
+import MSP_CMN
+from Recorder import Recorder
+from QISR import *
 
 URL = "http://openapi.xfyun.cn/v2/aiui"
 APPID = "93358e84"
 API_KEY = "bd963c2e6087ac4106b563b725815470"
 AUE = "raw"
 AUTH_ID = 'f87567f2159b425795ebb7ba9bc406ec'
-DATA_TYPE = "text"
+DATA_TYPE = "audio"
 SAMPLE_RATE = "16000"
 SCENE = "main_box"
 RESULT_LEVEL = "complete"
@@ -57,5 +61,19 @@ def readFile(filePath):
     data = binfile.read()
     return data
 
-r = requests.post(URL, headers=buildHeader(), data='一英里是多少公里?'.encode('utf8'))
-print(json.loads(r.content))
+
+
+if __name__ == '__main__':
+    msp_cmn = MSP_CMN()
+    msp_cmn.Login()
+ 
+    recorder = Recorder()
+
+    isr = QISR(msp_cmn.dll, recorder, ASR_RES_PATH, GRM_FILE, GRM_BUILD_PATH)
+    total_audio_data, has_spoken = recorder.get_record_audio_with_vad()
+    
+    start = time.time()
+    r = requests.post(URL, headers=buildHeader(), data=total_audio_data)
+    
+    print(json.loads(r.content))
+    print(time.time() - start)
