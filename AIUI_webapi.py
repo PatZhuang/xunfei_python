29c312f55df2a5371ebcd7c9401b8bca5b37ae0b
pat_zhuang <pat_zhuang@outlook.com>
2021-08-11 14:17:27 +0800

    update test script

--- AIUI_webapi.py
+++ AIUI_webapi.py
@@ -6,15 +6,11 @@ import hashlib
 import base64
 from rich import print
 import json
-import MSP_CMN
 from Recorder import Recorder
-from QISR import *
+from AIUI_CMN import APPID, API_KEY, AUTH_ID
 
 URL = "http://openapi.xfyun.cn/v2/aiui"
-APPID = "93358e84"
-API_KEY = "bd963c2e6087ac4106b563b725815470"
 AUE = "raw"
-AUTH_ID = 'f87567f2159b425795ebb7ba9bc406ec'
 DATA_TYPE = "audio"
 SAMPLE_RATE = "16000"
 SCENE = "main_box"
@@ -64,16 +60,14 @@ def readFile(filePath):
 
 
 if __name__ == '__main__':
-    msp_cmn = MSP_CMN()
-    msp_cmn.Login()
- 
     recorder = Recorder()
-
-    isr = QISR(msp_cmn.dll, recorder, ASR_RES_PATH, GRM_FILE, GRM_BUILD_PATH)
     total_audio_data, has_spoken = recorder.get_record_audio_with_vad()
     
-    start = time.time()
-    r = requests.post(URL, headers=buildHeader(), data=total_audio_data)
-    
-    print(json.loads(r.content))
-    print(time.time() - start)
+    if total_audio_data != b'':
+        start = time.time()
+        r = requests.post(URL, headers=buildHeader(), data=total_audio_data)
+        
+        print(json.loads(r.content))
+        print(time.time() - start)  
+    else:
+        print('no audio input.')
