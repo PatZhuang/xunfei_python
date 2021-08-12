a3524f8521f4f99f6e0f68ba2c84d6f67fa8faf9
pat_zhuang <pat_zhuang@outlook.com>
2021-08-12 23:30:22 +0800

    update QIVW.py to correspond to iflytek docs

--- QIVW.py
+++ QIVW.py
@@ -1,4 +1,5 @@
 from ctypes import *
+from struct import error
 from Recorder import Recorder
 from MSP_TYPES import *
 from rich import print
@@ -13,8 +14,8 @@ class QIVW(object):
         self.recorder = recorder
         self.ivw_threshold = '0:1450,1:1450,2:1450,3:1450'
         self.jet_path = 'fo|res/ivw/wakeupresource.jet'
-        self.sessionID = c_void_p()
-        self._session_avail = False
+        self.sessionID = c_char_p()
+        self._session_valid = False # mark if sessionID is valid
         self.awoken = False
         
         # callback
@@ -26,13 +27,13 @@ class QIVW(object):
                 return
             elif MSP_IVW_MSG_WAKEUP == msg:
                 # info 是唤醒结果首地址，param2 是结果长度，结合两者可以读出数据
-                info_str = (c_char * param2).from_address(info).value.decode('utf-8')
+                info_str = read_charp_with_len(info, param2).value.decode('utf-8')
                 print("info =>", info_str)
             elif MSP_IVW_MSG_ISR_RESULT == msg:
                 # 按照文档的描述，实际上 Linux 的 SDK 中无法使用 oneshot 模式唤醒
                 # 也就是不能做 唤醒+识别 因此以下两个判断应该不会成立
                 if param1 == MSP_REC_STATUS_SUCCESS:
-                    raw_info = read_pointer(info, param2)
+                    raw_info = read_charp_with_len(info, param2)
                     info_str = b''.join(list(raw_info)).decode('utf-8')
                     print("info =>", info_str)
             elif MSP_IVW_MSG_ISR_EPS == msg:
@@ -53,35 +54,46 @@ class QIVW(object):
         self.dll.QIVWSessionBegin.restype = c_char_p
         
     def SessionBegin(self):
-        errorCode = c_int64()
-        beginParams = "ivw_threshold={},sst=oneshot,ivw_res_path={},ivw_shot_word=0".format(self.ivw_threshold, self.jet_path)
-        beginParams = bytes(beginParams, encoding="utf8")
+        error_code = c_int64()
+        begin_params = "ivw_threshold={},sst=oneshot,ivw_res_path={},ivw_shot_word=0".format(self.ivw_threshold, self.jet_path)
+        begin_params = bytes(begin_params, encoding="utf8")
         
-        self.sessionID = self.dll.QIVWSessionBegin(None, beginParams, byref(errorCode))
-        if MSP_SUCCESS != errorCode.value:
-            raise RuntimeError("QIVWSessionBegin failed, error code %d" % errorCode.value)
-        self._session_avail = True
+        self.sessionID = self.dll.QIVWSessionBegin(None, begin_params, byref(error_code))
+        if MSP_SUCCESS != error_code.value:
+            raise RuntimeError("QIVWSessionBegin failed, error code %d" % error_code.value)
+        self._session_valid = True
+        return self.sessionID
         
-    def RegisterNotify(self):
-        ret = self.dll.QIVWRegisterNotify(self.sessionID, self.pIVWCallbackFunc, None)
+    def RegisterNotify(self, sessionID=None, msg_proc_cb=None, user_data=None):
+        if not sessionID:
+            sessionID = self.sessionID
+        assert sessionID is not None, 'SessionID is None'
+        if not msg_proc_cb:
+            msg_proc_cb = self.pIVWCallbackFunc
+        ret = self.dll.QIVWRegisterNotify(sessionID, msg_proc_cb, user_data)
         if MSP_SUCCESS != ret:
             raise RuntimeError("QIVWRegisterNotify failed, error code: %d" % ret)
         
-    def AudioWrite(self, audioData, audioLen):
-        ret = self.dll.QIVWAudioWrite(self.sessionID, audioData, audioLen, 2)
+    def AudioWrite(self, audioData, audioLen, audio_status=2):
+        ret = self.dll.QIVWAudioWrite(self.sessionID, audioData, audioLen, audio_status)
         if MSP_SUCCESS != ret:
             raise RuntimeError("QIVWAudioWrite failed, errCode: %d", ret)
         
-    def SessionEnd(self):
-        hints = "Done wakeup"
-        hints = bytes(hints, encoding="utf8")
+    def SessionEnd(self, hints="Done wakeup"):
+        if type(hints) is str:
+            hints = hints.encode('utf8')
         ret = self.dll.QIVWSessionEnd(self.sessionID, hints)
         if MSP_SUCCESS != ret:
             raise RuntimeError("QIVWSessionEnd Error, errCode: %d" % ret)
-        self.sessionID = c_void_p()
-        self._session_avail = False
+        self.sessionID = c_char_p()
+        self._session_valid = False
         
     def wakeup(self):
+        """一次完整的唤醒流程
+
+        Returns:
+            bool: 是否正确结束本次唤醒
+        """
         try:
             self.SessionBegin()
             self.RegisterNotify()
@@ -99,12 +111,13 @@ class QIVW(object):
             return False
     
     def debug(self):
-        # 连续检测唤醒
+        """连续测试唤醒
+        """
         while True:
             self.wakeup()
             
     def __del__(self):
-        if self._session_avail:
+        if self._session_valid:
             try:
                 self.SessionEnd()
             except RuntimeError as e:
